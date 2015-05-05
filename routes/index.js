09cccd2893a492d7d67a7ec2bc2848421d8f76ef
Anas Faris <anasfaris@yahoo.com>
2015-05-05 03:33:29 -0400

    Switch from jade to ejs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+var persons = {
+    "data": [
+    {
+        "name": "Syed Mokhtar",
+        "target": 5
+    }, {
+        "name": "Imran Ariffin WOW",
+        "target": 1
+    }, {
+        "name": "Anas Faris",
+        "target": 2
+    }
+    ]
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: 'UT Darul Hikmah',
+        persons: persons
+    });
 });
 
 module.exports = router;
