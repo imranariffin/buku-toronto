18f758e62f01b4a4d2eb3f937f0a2e3c2ebc8a7e
imranariffin <imran.ariffin@mail.utoronto.ca>
2015-05-05 17:58:05 -0400

    adds user rank page, sorts users according to scores

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,61 @@
 var express = require('express');
 var router = express.Router();
 
+var title = "Rank Userszzz";
+var users = {
+    "data": [
+    {
+        "name": "Syed Mokhtar",
+        "target": 5,
+        "score" : 100
+    }, {
+        "name": "Imran Ariffin WOW",
+        "target": 1,
+        "score" : 75
+    }, {
+        "name": "Anas Faris",
+        "target": 2,
+        "score" : 89
+    }
+    ]
+}
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('respond with a resource');
+  res.render('users', {
+  		title: title,
+  		users: rankUsers(users)
+  });
 });
 
 module.exports = router;
+
+function rankUsers(userData)
+{
+	var users = userData["data"];
+	users.sort(function(a, b) {
+		return b["score"] - a["score"];
+	});
+
+	userData["data"] = users;
+
+	return userData;
+}
+
+function replaceData(users)
+{
+	var data = users["data"];
+	var newData = [{        
+		"name": "Saiful Bukhari",
+        "target": 100,
+        "score" : 9999
+    }];
+
+    users["data"] = newData;
+
+    console.log(users["data"]);
+    console.log(users);
+
+    return users;
+}
